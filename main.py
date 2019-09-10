9a36c7b09994956a705ecf0c77cf189f7fe021cc
Nolan Blankenau <nolanblankenau@gmail.com>
2019-09-10 14:26:05 -0500

    Added createRects method which will create an 8x8 grid

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+import pygame.gfxdraw
 from pygame.locals import *
+import random
 
 
 pygame.init()
@@ -8,20 +10,38 @@ disp_width = 1080
 disp_height = 720
 
 disp = pygame.display.set_mode((disp_width, disp_height))
+disp.fill((255, 255, 255))
 pygame.display.set_caption('Battleship')
 
 
 def event_handler():
     for event in pygame.event.get():
-        if event.type == QUIT or (
-             event.type == KEYDOWN and (
-              event.key == K_ESCAPE or
-              event.key == K_q
-             )):
+        if event.type == QUIT or ( event.type == KEYDOWN and ( event.key == K_ESCAPE or event.key == K_q)):
             pygame.quit()
             quit()
 
+def createRects(x, y):
+    """Creates an 8x8 grid of squares
+    
+    Keyword Arguments:
+    x -- the x position for the top right corner of the grid to start at
+    y -- the y position for the top right corner of the grid to start at
+    """
+    interval = (disp_width / 2) / 16
+    divX = interval + x
+    divY = interval + y
+    rects = [[0 for x in range(8)] for y in range(8)] 
+    for i in range(0, 8):
+        for j in range(0, 8):
+            rects[i][j] = pygame.Rect(divX, divY, interval, interval)
+            pygame.draw.rect(disp, (0, 0, 0), rects[i][j], 2)
+            divX += interval
+        divX = interval + x
+        divY += interval
+
 while True:
     event_handler()
-    pygame.draw.circle(disp, (0,0,255), (150, 50), 30, 1)
+    createRects(200, 200)
+    createRects(500, 200)
+
     pygame.display.update()
