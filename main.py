8f703e63ffa76440816dee83ebb292476a3af553
Nolan Blankenau <nolanblankenau@gmail.com>
2019-09-10 15:36:06 -0500

    Added trackRects method that will change grid squares red if you press on them. I also, deleted the index.html file

--- main.py
+++ main.py
@@ -11,22 +11,44 @@ disp_height = 720
 
 disp = pygame.display.set_mode((disp_width, disp_height))
 disp.fill((255, 255, 255))
-pygame.display.set_caption('Battleship')
+pygame.display.set_caption('Battleboats')
 
 
 def event_handler():
+    """Checks for different pygame events"""
+
     for event in pygame.event.get():
         if event.type == QUIT or ( event.type == KEYDOWN and ( event.key == K_ESCAPE or event.key == K_q)):
             pygame.quit()
             quit()
 
+def isPointInRect(x, y, rect):
+    """Checks if a coordinate is within the bounds of a pygame.rect object
+
+    Args:
+    x (float): x coordinate to check
+    y (float): y coordinate to check
+    rect (pygame.Rect): object to see if x any y are in
+
+    Returns:
+        bool: True if x and y are in rect, False otherwise
+    """
+
+    if x < rect.x + rect.width and x > rect.x and y < rect.y + rect.height and y > rect.y:
+        return True
+    return False
+
 def createRects(x, y):
     """Creates an 8x8 grid of squares
     
-    Keyword Arguments:
-    x -- the x position for the top right corner of the grid to start at
-    y -- the y position for the top right corner of the grid to start at
+    Args:
+    x (int): the x position for the top right corner of the grid to start at
+    y (int): the y position for the top right corner of the grid to start at
+
+    Returns:
+        8x8 array of pygame.Rect objects
     """
+
     interval = (disp_width / 2) / 16
     divX = interval + x
     divY = interval + y
@@ -39,9 +61,36 @@ def createRects(x, y):
         divX = interval + x
         divY += interval
 
+    pygame.display.update()
+    return rects
+
+def trackRects(rects):
+    """Tracks when a single square in a grid is pressed by the mouse
+
+    Args:
+        rects (8x8 array of pygame.Rect objects): the grid to check on
+    """
+
+    newPress = True
+    mouseX = 0
+    mouseY = 0
+
+    if pygame.mouse.get_pressed() == (1, 0, 0) and newPress:
+        newPress = False
+        mouseX, mouseY = pygame.mouse.get_pos()
+        for i in range(0, 8):
+            for j in range(0, 8):
+                if isPointInRect(mouseX, mouseY, rects[i][j]):
+                    pygame.draw.rect(disp, (255, 0, 0), rects[i][j])
+                    pygame.display.update(rects[i][j])
+
+    elif pygame.mouse.get_pressed() != (1, 0, 0):
+        newPress = True
+
+
+leftGrid = createRects(200, 200)
+rightGrid = createRects(500, 200)
+
 while True:
     event_handler()
-    createRects(200, 200)
-    createRects(500, 200)
-
-    pygame.display.update()
+    trackRects(leftGrid)
