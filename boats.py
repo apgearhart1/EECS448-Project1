1aec068815be25a383ac28354d441324588e36cc
apgearhart1 <apgearhart1@gmail.com>
2019-09-12 23:02:36 -0500

    made validPlace() in boat.py more robust and checks consecutivity on the indexes of boat placement

--- boats.py
+++ boats.py
@@ -5,16 +5,60 @@ class boat:
         self.destroyed = False
         self.hitlist = []
     def validPlace(self, coordinates):
-        for i in range(0, len(coordinates)):
-            if i != (len(coordinates) - 1):
-                if (coordinates[i][0] == coordinates[i+1][0]) or (coordinates[i][1] == coordinates[i+1][1]):
-                    return False
-            else:
-                if (coordinates[i][0] != coordinates[i-1][0]) or (coordinates[i][1] == coordinates[i-1][1]):
-                    return False
+        """Checks to see if boat is in a valid place and if each part of the boat is at consecutive indexes
+
+        Args:
+        coordinates - array of coordinate that the opponent wants to hit
+
+        Returns:
+        Returns a bool if the boat is in a valid place, prevents being place diagonally
+        """
+        if len(coordinates) != 1:
+            for i in range(0, len(coordinates)):
+                check = False #if true, program won't accidentally check the second condition
+                if i != (len(coordinates) - 1):
+                    if (coordinates[i][0] != coordinates[i+1][0]):
+                        print(1, coordinates[i][0], coordinates[i+1][0])
+                        return False
+                    check = True
+                    if (coordinates[i][1] != coordinates[i+1][1] and check == False):
+                        print(2, coordinates[i][1], coordinates[i+1][1])
+                        return False
+                else:
+                    if (coordinates[i][0] != coordinates[i-1][0]):
+                        print(3, coordinates[i][0], coordinates[i-1][0])
+                        return False
+                    check = True
+                    if (coordinates[i][1] != coordinates[i-1][1] and check == False):
+                        print(4, coordinates[i][1], coordinates[i-1][1])
+                        return False
+            if coordinates[0][0] == coordinates[1][0]:
+                for i in range(len(coordinates)):
+                    if i != len(coordinates)-1:
+                        if coordinates[i+1][1] - coordinates[i][1] != 1:
+                            return False
+                    else:
+                        if coordinates[i][1] - coordinates[i-1][1] != 1:
+                            return False
+            elif coordinates[0][1] == coordinates[1][1]:
+                for i in range(len(coordinates)):
+                    if i != len(coordinates)-1:
+                        if coordinates[i+1][0] - coordinates[i][0] != 1:
+                            return False
+                    else:
+                        if coordinates[i][0] - coordinates[i-1][0] != 1:
+                            return False
+        else:
+            if coordinates[0][0] < 0 or coordinates[0][0] >= 8 or coordinates[0][1] < 0 or coordinates[0][1] >= 8:
+                return False
         return True
 
     def checkDestroyed(self):
+        """Checks to see if boat is destroyed
+
+        Returns:
+        Returns a bool if the boat is destroyed (True) or not (False)
+        """
         if(len(self.hitlist) == self.size):
             self.destroyed = True
             return True
@@ -22,6 +66,14 @@ class boat:
             self.destroyed = False
             return False
     def hit(self, coordinate):
+        """Adds coordinate to hitlist array and checks if it is alread hit
+
+        Args:
+        coordinates - array of coordinate that the opponent wants to hit
+
+        Returns:
+        Returns a string to let the player know if it was a hit or not
+        """
         if coordinate not in self.coordinates:
             return "Miss!"
         else:
@@ -33,6 +85,16 @@ class boat:
             else:
                 return "Hit Confirmed!"
     def getCoordinates(self):
+        """gives user coordinates of the boat
+
+        Returns:
+        Returns a 2D array of the coordinates
+        """
         return self.coordinates
     def getSize(self):
+        """Returns size of the boat
+
+        Returns:
+        Returns int of the size of the boat
+        """
         return self.size
