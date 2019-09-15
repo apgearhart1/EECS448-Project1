518c968db6d6578c6e353efdc0d4f2db0b0d952f
Nick North <jnnorth1998@gmail.com>
2019-09-15 17:22:36 -0500

    Updated function definitions for player class and added imports

--- player.py
+++ player.py
@@ -1,3 +1,7 @@
+from board import board 
+from boats import boat
+
+
 class Player:
 	def __init__(self, myBoard, theirBoard):
 		# this player's list of ships
@@ -7,9 +11,17 @@ class Player:
 		# opponent's board		
 		self.theirBoard = theirBoard 
 
+	def shipsDestroyed(self):
+		counter = 0
+		for i in self.shipList:
+			if shelf.shipList[i].checkDestroyed():
+				counter = counter + 1
+				
+	
+		return counter
 	
 
-	def placeShip(length):
+	def placeShip(self, length):
 		# print coordinates and store them in a tuple
 		print("Select x coordinate: ")
 		x = input()
@@ -19,21 +31,18 @@ class Player:
 		# creates a new ship
 		tempShip = boat(length, location)
 		
-		if tempShip.validPlace(length, location):
+		if tempShip.validPlace(self, length, location):
 			self.shipList.append(tempShip)
 			# place ship on board here
-			return "Player ship placed!"
+			return True
 			
 		else:
-			return "Ship could not be placed!"
+			return False
 		
 		
-	#def attack(self, location):
-		# write code that uses board methods
-		#for i in location:
-			#for j in location:
-				# attack opponent board at i,j  
-		
-	#def shipsDestroyed():
-		#for i in self.shipList
-			# put board code here 
+	def attack(self, location):
+		 #write code that uses board methods
+		for i in location:
+			for j in location:
+				#attack opponent board at i,j  
+	
