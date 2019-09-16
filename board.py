34d6f64f4e30c70a8f2b5e4027f91e205e362ef6
Nick North <jnnorth1998@gmail.com>
2019-09-16 13:32:32 -0500

    Added test function for printing board

--- board.py
+++ board.py
@@ -1,22 +1,26 @@
 class board:
-    def __init__(self, rowSize, colSize):
-        self.rowSize = rowSize
+	def __init__(self, rowSize, colSize):
+		self.rowSize = rowSize
 		self.colSize = colSize
 		self.boardStorage = [[0 for y in range(colSize)] for x in range(rowSize)]
-        self.hits = [[]]
-        self.misses = [[]]
+		self.hits = [[]]
+		self.misses = [[]]
 		
-    def isMiss(self, xPos, yPos):
-        if(self.misses[xPos][yPos] == True):
-            return True
+	def isMiss(self, xPos, yPos):
+		if(self.misses[xPos][yPos] == True):
+			return True
         
-        return False
+		return False
 		
-    def isHitt(self, xPos, yPos):
-        if(self.hits[xPos][yPos] == True):
-            return True
+	def isHitt(self, xPos, yPos):
+		if(self.hits[xPos][yPos] == True):
+			return True
+		else:
+			return False
         
-        return False
-        
-    def getSize(self):
-        return (self.rowSize, self.colSize)
+	def getSize(self):
+		return (self.rowSize, self.colSize)
+		
+	def testBoard(self):
+		b = [[0 for y in range(8)] for x in range(8)]
+		print(b)
