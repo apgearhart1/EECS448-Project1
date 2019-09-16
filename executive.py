c13e0d922e79b4f1879c4b376da6287bea404fdd
Nick North <jnnorth1998@gmail.com>
2019-09-16 16:04:49 -0500

    Added attack method to executive class

--- executive.py
+++ executive.py
@@ -1,4 +1,6 @@
-from player import Player
+#from player import Player
+#from boat import boat
+#from board import board
 
 class Executive:
 
@@ -7,10 +9,10 @@ class Executive:
 		self.player2 = Player()
 		self.turns = 1
 	
-	def select():	
+	def select(self):	
 		check = False
 		while not check:
-			print("Select number of boats (1-5): ")
+			print("Select number of boats (1-5)")
 			num = input()
 			if num > 0 and num < 6:
 				for i in range(num):
@@ -21,7 +23,7 @@ class Executive:
 			else:
 				print("Invalid number of boats!")
 		
-	def turn(self):
+	def playerTurn(self):
 		if self.turns % 2 == 0:
 			self.turns += 1
 			return "player2"
@@ -30,4 +32,18 @@ class Executive:
 			self.turns += 1
 			return "player1"
 		
-		
+	def attack(self):
+		if self.playerTurn() == "player1":
+			#check where the player clicks here 
+			#location = where the player clicks
+			tempShipList = self.player2.getShipList()
+			for i in tempShipList:
+				if self.player2.getShip(i).getcoordinates() == location:
+					self.player2.getShip(i).hit(location)
+		elif self.playerTurn() == "player2":
+			tempShipList = self.player1.getShipList()
+			for i in tempShipList:
+				if self.player1.getShip(i).getcoordinates() == location:
+					self.player1.getShip(i).hit(location)
+					
+			
