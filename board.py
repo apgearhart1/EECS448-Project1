a114ec28952637542e8d5704a7f9ebb00df6744a
Harrison Heeb <harrisonheeb@Harrisons-MacBook-Pro.local>
2019-09-16 14:08:37 -0500

    Add isBoat method
    Created an isBoat method used to check if there is a boat at the
    position where a player is trying to place their boat.

--- board.py
+++ board.py
@@ -1,8 +1,11 @@
 class board:
-	def __init__(self, rowSize, colSize):
+	def __init__(self, rowSize, colSize, boatlist):
 		self.rowSize = rowSize
 		self.colSize = colSize
 		self.boardStorage = [[0 for y in range(colSize)] for x in range(rowSize)]
+        for boat in boatlist:
+            for coordinate in boat.getCoordinates():
+                self.boardStorage[coordinate[0]][coordinate[1]] = 1
 		self.hits = [[]]
 		self.misses = [[]]
 		
@@ -24,3 +27,10 @@ class board:
 	def testBoard(self):
 		b = [[0 for y in range(8)] for x in range(8)]
 		print(b)
+
+    def isBoat(xPos, yPos):
+        if(self.boardStorage[xPos][yPos] == 1):
+            return True
+        return False
+
+
