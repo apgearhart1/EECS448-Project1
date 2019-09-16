994fb754b47a50364143466dc455d29bc157404c
Nick North <jnnorth1998@gmail.com>
2019-09-16 15:18:49 -0500

    Added function for player turns

--- executive.py
+++ executive.py
@@ -5,15 +5,29 @@ class Executive:
 	def __init__(self):
 		self.player1 = Player()
 		self.player2 = Player()
+		self.turns = 1
 	
-	def select():
-		print("Select number of boats")
-		num = input()
+	def select():	
+		check = False
+		while not check:
+			print("Select number of boats (1-5): ")
+			num = input()
+			if num > 0 and num < 6:
+				for i in range(num):
+					self.player1.placeShip(i)
+				for j in range(num):
+					self.player2.placeShip(j)
+				check = True
+			else:
+				print("Invalid number of boats!")
 		
-		if num > 0 and num < 6:
-			for i in range(num):
-				self.player1.placeShip(i)
-			for j in range(num):
-				self.player2.placeShip(j)
+	def turn(self):
+		if self.turns % 2 == 0:
+			self.turns += 1
+			return "player2"
+			
+		else:
+			self.turns += 1
+			return "player1"
 		
 		
