7eb298c0285a53156c7e9b746d940755a58dde25
Nick North <jnnorth1998@gmail.com>
2019-09-20 14:27:29 -0500

    Added documentation to methods of the player class

--- player.py
+++ player.py
@@ -13,15 +13,29 @@ class Player:
 		self.opBoard = board(8,8,[])
 
 	def shipsDestroyed(self):
+		"""Checks to see how many ships have been destroyed
+
+
+        Returns:
+        Returns the number of a player's ships that have been destroyed
+        """
 		# increment counter for every ship that is destroyed 
 		counter = 0
-		for i in self.shipList:
-			if shelf.shipList[i].checkDestroyed():
+		for ship in self.shipList:
+			if self.shipList[ship].checkDestroyed():
 				counter = counter + 1
 		return counter
 	
 
-	def placeShip(self, ship):			
+	def placeShip(self, ship):		
+		"""Places a ship on the player's board
+
+        Args:
+        ship - the ship object that needs to be placed on the board
+
+        Returns:
+        Returns true if the ship could be placed, false otherwise
+        """
 		if ship.validPlace(ship.getCoordinates()):
 			self.shipList.append(ship)
 			self.shipCoordinateList.append(ship.getCoordinates())
@@ -32,15 +46,41 @@ class Player:
 			return False
 			
 	def getShipList(self):
+		"""Gets the list of ships for this player
+
+        Returns:
+        Returns this player's list of ships
+        """
 		return self.shipList
 		
 	def getShip(self, index):
+		"""Gets a specific ship in the player's list, ships should be organised by length i.e. a ship of size 3 is in the 3rd index 
+
+        Args:
+        index - the index of the player's ship
+
+        Returns:
+        Returns the ship object we want
+        """
 		return self.shipList[index]
 		
 	def getCoordinateList(self):
+		"""Gets this player's current list of ship coordinates
+
+        Returns:
+        Returns the list of ship coordinates
+        """
 		return self.shipCoordinateList 
 		
 	def removeShip(coordinates):
+		"""Removes ship from the players current list of ships and list of ship coordinates 
+
+        Args:
+        coordinates - coordinates of the ship we want to remove 
+
+        Returns:
+        Returns true if the ship was successfully removed, false otherwise
+        """
 		for ship in self.ShipList:
 			if self.shipList[ship].getCoordinates() == coordinates:
 				self.shipList.pop(ship)
